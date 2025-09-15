d343c778e356bf0f86bd7a011bd5d5e66e1fa49d
Nataliia K <nbezverkhaya@gmail.com>
2025-09-15 18:52:19 +0200

    Add pytest hook to capture screenshots on test failures and embed them in HTML report

--- framework_design/conftest.py
+++ framework_design/conftest.py
@@ -1,6 +1,9 @@
 import pytest
 from selenium import webdriver
 
+import os
+driver = None
+
 def pytest_addoption(parser):
     parser.addoption(""
         "--browser_name", action="store", default="firefox", help="browser selection"
@@ -8,6 +11,7 @@ def pytest_addoption(parser):
 
 @pytest.fixture(scope="function")
 def browser_instance(request):
+    global driver
     browser_name = request.config.getoption("--browser_name")
     if browser_name == "chrome":
         driver = webdriver.Chrome()
@@ -17,3 +21,44 @@ def browser_instance(request):
     driver.implicitly_wait(5)
     yield driver
     driver.quit()
+
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item):
+    """
+    Extends the PyTest Plugin to take and embed screenshot in html report,
+    whenever a test fails.
+    :param item:
+    """
+    pytest_html = item.config.pluginmanager.getplugin('html')
+    outcome = yield
+    report = outcome.get_result()
+    extra = getattr(report, 'extra', [])
+
+    if report.when == 'call' or report.when == 'setup':
+        xfail = hasattr(report, 'wasxfail')
+        if (report.skipped and xfail) or (report.failed and not xfail):
+            reports_dir = os.path.join(os.path.dirname(__file__), 'reports')
+            os.makedirs(reports_dir, exist_ok=True)
+
+            file_name = os.path.join(
+                reports_dir, report.nodeid.replace("::", "_") + ".png"
+            )
+
+            print("file name is " + file_name)
+
+            driver = item.funcargs.get("browser_instance", None)
+            if driver is not None:
+                driver.save_screenshot(file_name)
+
+            if file_name:
+                html = (
+                    '<div><img src="%s" alt="screenshot" '
+                    'style="width:304px;height:228px;" '
+                    'onclick="window.open(this.src)" align="right"/></div>'
+                ) % file_name
+                extra.append(pytest_html.extras.html(html))
+        report.extra = extra
+
+def capture_screenshot(file_name):
+    driver.get_screenshot_as_a_file(file_name)
